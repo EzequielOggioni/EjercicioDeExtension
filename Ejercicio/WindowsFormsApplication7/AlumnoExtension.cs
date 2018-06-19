3e40093487fc99d3397acf8f4611d82f50a2be4e
profesor <profesor>
2018-06-18 22:21:12 -0300

    resuelto sin guardar

---
+++ Ejercicio/WindowsFormsApplication7/AlumnoExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication7
+{
+    public static class AlumnoExtension
+    {
+        public static void Guardar(this Alumno al)
+        {
+
+        }
+
+    }
+}
